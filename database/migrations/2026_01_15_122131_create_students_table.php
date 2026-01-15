f0f822834cdb22c69760266ec4ffad09dbd6a427
Athena Casiño <169009435+AthenaMTC@users.noreply.github.com>
2026-01-15 20:22:50 +0800

    Add student table migration to database

---
+++ database/migrations/2026_01_15_122131_create_students_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->foreignId('college_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->foreignId('course_id')
+                ->nullable()
+                ->constrained()
+                ->nullOnDelete();
+
+            $table->foreignId('year_level_id')
+                ->nullable()
+                ->constrained()
+                ->nullOnDelete();
+
+            $table->foreignId('section_id')
+                ->nullable()
+                ->constrained()
+                ->nullOnDelete();
+
+            $table->string('last_name');
+            $table->string('first_name');
+            $table->string('middle_name');
+            $table->string('contact')->nullable();
+            $table->string('email')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
