7ae2e2e9ff7a8355f840160836a6791734d3cf61
Athena Casiño <169009435+AthenaMTC@users.noreply.github.com>
2026-01-21 01:04:26 +0800

    add x-cloak to new student modal to hide it until Alpine initializes

--- resources/views/college/students.blade.php
+++ resources/views/college/students.blade.php
@@ -152,8 +152,8 @@
     </div>
 
     {{-- New Student Modal --}}
-    <div x-show="showModal" class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-        <div @click.away="showModal = false" class="bg-white rounded-lg shadow-lg w-full max-w-lg relative p-6">
+    <div x-show="showModal" x-cloak class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50" >
+        <div @click.away="showModal = false" class="bg-white rounded-lg shadow-lg w-full max-w-lg relative p-6" >
             <button @click="showModal = false" class="absolute top-2 right-3 text-gray-500 hover:text-gray-800 text-xl">&times;</button>
 
             <h3 class="text-lg font-semibold mb-4 border-b pb-2">New Student</h3>
